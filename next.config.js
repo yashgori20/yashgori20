ca15f30612718a7d818de850c03052e3ff896318
Yash Nilesh Gori <yashnileshgori@gmail.com>
2025-06-15 05:27:03 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Replace 'your-repo-name' with your actual repository name
+  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+}
+
+module.exports = nextConfig

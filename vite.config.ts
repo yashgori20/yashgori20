bdbf0d6ac1edc4ebe733d13cc384f5a03f19b58f
Yash Nilesh Gori <yashnileshgori@gmail.com>
2025-06-15 05:43:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // GitHub Pages deployment configuration
+  base: mode === 'production' ? '/yashgori20/' : '/',
 }));

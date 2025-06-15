add23b5648a3f83a7790af28aed54df7ab99a1f9
Yash Nilesh Gori <yashnileshgori@gmail.com>
2025-06-16 00:20:55 +0530

    fgsdfg

--- src/hooks/usePageNavigation.ts
+++ src/hooks/usePageNavigation.ts
@@ -14,11 +14,11 @@ export const usePageNavigation = () => {
   const changePage = useCallback((newIndex: number) => {
     if (isAnimating.current || !windowHeight) return;
     const clampedIndex = Math.max(0, Math.min(newIndex, views.length - 1));
-    // if (clampedIndex !== pageIndex) {
-    //   isAnimating.current = true;
-    //   setPageIndex(clampedIndex);
-    //   setActiveView(views[clampedIndex]);
-    // }
+    if (clampedIndex !== pageIndex) {
+      isAnimating.current = true;
+      setPageIndex(clampedIndex);
+      setActiveView(views[clampedIndex]);
+    }
   }, [pageIndex, windowHeight]);
 
   useEffect(() => {
